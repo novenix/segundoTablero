d296a1f75fb469b8201ff7802ec8cf8bd8ea9ea5
Nicolas Torres <Nicolas.torres-p@mail.escuelaing.edu.co>
2021-06-29 12:51:17 -0500

    comit inicial

---
+++ src/main/java/com/example/triki/controllers/DrawingServiceController.java
@@ -0,0 +1,16 @@
+package com.example.triki.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DrawingServiceController {
+
+    @GetMapping("/status")
+    public String status() {
+        return "{\"status\":\"Greetings from Spring Boot. " +
+                java.time.LocalDate.now() + ", " +
+                java.time.LocalTime.now() +
+                ". " + "The server is Runnig!\"}";
+    }
+}
---
+++ src/test/java/com/example/triki/TrikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
